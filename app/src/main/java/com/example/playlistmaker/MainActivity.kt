4c8bb2d7ed7630552680c1c17c788c9bfcd0ff16
Artem Vazerov <avazerov@inno.tech>
2024-02-04 19:07:17 +0300

    Сделал часть обработчиков

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val search = findViewById<Button>(R.id.search)
+        val library = findViewById<Button>(R.id.library)
+        val settings = findViewById<Button>(R.id.settings)
+
+        val searchIntent = Intent(this, SearchActivity::class.java)
+        // Первый способ, через анонимную функцию
+        val imageClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                startActivity(searchIntent)
+            }
+        }
+        search.setOnClickListener(imageClickListener) // передали в лисенер объект кнопки
+
+        library.setOnClickListener {
+            val libIntent = Intent(this, LibraryActivity::class.java)
+            startActivity(libIntent)
+        }
+
+        settings.setOnClickListener{
+            val settingsIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(settingsIntent)
+        }
+
+
     }
 }
